44a7c02cac6f79f243711cc1e8a66ab97f70b0a4
Bowser1704 <bowser1704@icloud.com>
2020-11-18 21:20:27 +0800

    lab partition management

---
+++ partition-manager/partition-manager.py
@@ -0,0 +1,76 @@
+import copy
+
+free_areas = []
+num = int(input())
+for i in range(num):
+    start, size = input().split()
+    free_areas.append([int(start), int(size)])
+
+requests = []
+num = int(input())
+for i in range(num):
+    size = int(input())
+    requests.append(size)
+
+
+def free(areas, size):
+    areas.sort(key=lambda x: x.start)
+    start = 0
+    end = max(areas, key=lambda x: x[0]+x[1])
+    i, j = start, end
+    for area in areas:
+        if start < area[0]:
+            releaseable_size = area[0] - start
+            if size <= releaseable_size:
+                areas.append(start, size)
+            else:
+                area[0] -= releaseable_size
+                area[1] += releaseable_size
+                size -= releaseable_size
+                start = area[0] + area[1]
+        
+
+def adaption(areas, requests, key=None, reverse=False):
+    for request in requests:
+        isAllocated = False
+        for area in areas:
+            if area[1] >= request:
+                area[0] += request
+                area[1] -= request
+                isAllocated = True
+                if key:
+                    areas.sort(key=key, reverse=reverse)
+                break
+        if not isAllocated:
+            # print(request, areas)
+            print("There is not enough free space to allocate {} bytes!".format(request))
+    print_res(areas)
+
+def print_res(areas):
+    # print("Free space")
+    for area in areas:
+        print("start: {} end: {} size: {}".format(area[0], area[0]+area[1], area[1]))
+
+def ff(free_areas, requests):
+    print("\nFirst Fit")
+    areas = copy.deepcopy(free_areas)
+    adaption(areas, requests)
+    
+
+def bf(free_areas, requests):
+    print("\nBest Fit")
+    areas = copy.deepcopy(free_areas)
+    areas.sort(key = lambda x: x[1])
+    adaption(areas, requests, key=lambda x: x[1])
+
+def wf(free_areas, requests):
+    print("\nWorst Fit")
+    areas = copy.deepcopy(free_areas)
+    areas.sort(key = lambda x: x[1], reverse=True)
+    adaption(areas, requests, key=lambda x: x[1], reverse=True)
+
+
+
+ff(free_areas, requests)
+bf(free_areas, requests)
+wf(free_areas, requests)
--- readme.md
+++ readme.md
@@ -1,2 +1,8 @@
 ## 华中师范大学-操作系统原理-2020-Fall
-lab
+## 存储管理
+
+1. 什么是Balady现象？试找出一个Balady现象的例子。
+
+   https://en.wikipedia.org/wiki/B%C3%A9l%C3%A1dy%27s_anomaly
+
+   针对于 FIFO page replacement algorithm
